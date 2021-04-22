0ada2086079ab831adf99df6ef7294b806263ac7
Josesosa0777 <josephsosa0777@gmail.com>
2021-04-22 23:00:04 +0000

    gem friendly id for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ApplicationRecord
     self.email.split(/@/).first
   end
   has_many :courses
+  
+  extend FriendlyId
+  friendly_id :email, use: :slugged
+
   after_create :assign_default_role
 
   def assign_default_role
