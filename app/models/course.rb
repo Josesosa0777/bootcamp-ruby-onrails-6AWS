5520e34812a6608c7d3ebcce82ca62d57def0a57
Josesosa0777 <josephsosa0777@gmail.com>
2021-04-28 16:35:38 +0000

    add count cache, validation, sort by price, popularity, rating, newest

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,9 +2,11 @@ class Course < ApplicationRecord
   validates :title, :short_description, :language, :price, :level, presence: true
   validates :description, presence: true, length: { :minimum => 5 }
   
-  belongs_to :user
+  belongs_to :user, counter_cache: true
+  #User.find_each { |user| User.reset_counters(user.id, :courses) }  
   has_many :lessons, dependent: :destroy
   has_many :enrollments
+  validates :title, uniqueness: true
   
   def to_s
     title
@@ -38,6 +40,14 @@ class Course < ApplicationRecord
   
   def bought(user)
     self.enrollments.where(user_id: [user.id], course_id: [self.id]).empty?
+  end
+  
+  def update_rating
+    if enrollments.any? && enrollments.where.not(rating: nil).any?
+      update_column :average_rating, (enrollments.average(:rating).round(2).to_f) #to float
+    else
+      update_column :average_rating, (0)
+    end
   end
   
 end
