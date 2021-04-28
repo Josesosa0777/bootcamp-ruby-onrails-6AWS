c1568f3fb46f77a6ad1ef1a9a4ed06a064d04ebd
Josesosa0777 <josephsosa0777@gmail.com>
2021-04-28 17:45:56 +0000

    Track User progress in a Course

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,6 +6,7 @@ class Course < ApplicationRecord
   #User.find_each { |user| User.reset_counters(user.id, :courses) }  
   has_many :lessons, dependent: :destroy
   has_many :enrollments
+  has_many :user_lessons, through: :lessons
   validates :title, uniqueness: true
   
   scope :latest, -> { limit(3).order(created_at: :desc) }
@@ -43,6 +44,12 @@ class Course < ApplicationRecord
   
   def bought(user)
     self.enrollments.where(user_id: [user.id], course_id: [self.id]).empty?
+  end
+  
+  def progress(user)
+    unless self.lessons_count == 0
+      user_lessons.where(user: user).count/self.lessons_count.to_f*100
+    end
   end
   
   def update_rating
